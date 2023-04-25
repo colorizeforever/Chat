179dc14fd3beedd14527724700961fa1d487832e
Myslimov <gamersondanil@gmail.com>
2023-04-25 23:39:53 +0300

    angular update, removed unused dependencies

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   bootstrap: [AppComponent],
 })
